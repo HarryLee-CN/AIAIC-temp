283ffe41d88cea4d33d6b9135a889c401a71ca2a
李秋奇@home <mail@qiuqi.li>
2024-03-21 21:43:43 +0800

    no message

--- src/pages/poster/index.vue
+++ src/pages/poster/index.vue
@@ -1,9 +1,27 @@
 <template>
   <div id="fullpage">
-    <div class="section">Some section</div>
-    <div class="section">Some section</div>
-    <div class="section">Some section</div>
-    <div class="section">Some section</div>
+    <div class="section">
+      活动介绍
+    </div>
+    <div class="section">
+      作品征集
+    </div>
+    <div class="section">
+      作品展示
+    </div>
+    <div class="section">
+      顾问团队
+    </div>
+    <div class="section">
+      合作支持
+    </div>
+  </div>
+  <div class="cells">
+    <div class="cell cell-1" @click="handleClick(1)" ref="cell-1">活动介绍</div>
+    <div class="cell cell-2" @click="handleClick(2)" ref="cell-2">作品征集</div>
+    <div class="cell cell-3" @click="handleClick(3)" ref="cell-3">作品展示</div>
+    <div class="cell cell-4" @click="handleClick(4)" ref="cell-4">顾问团队</div>
+    <div class="cell cell-5" @click="handleClick(5)" ref="cell-5">合作支持</div>
   </div>
 </template>
 
@@ -15,18 +33,72 @@ export default {
   },
   computed: {},
   methods: {
-    handleClick() {
-      uni.navigateTo({
-        url: "/pages/poster/index"
-      })
+    handleClick(e) {
+      alert(e)
     }
   },
   mounted() {
+    const viewportHeight1 = document.body.clientHeight - 56 * 5
+    const viewportHeight2 = document.body.clientHeight - 56 * 4
+    const viewportHeight3 = document.body.clientHeight - 56 * 3
+    const viewportHeight4 = document.body.clientHeight - 56 * 2
+
+    this.$refs['cell-1'].style = `top: 0`;
+    this.$refs['cell-2'].style = `top: ${viewportHeight1 + 56}px`;
+    this.$refs['cell-3'].style = `top: ${viewportHeight2 + 56}px`;
+    this.$refs['cell-4'].style = `top: ${viewportHeight3 + 56}px`;
+    this.$refs['cell-5'].style = `top: ${viewportHeight4 + 56}px`;
+    
+
     const fullpage = window.fullpage
     new fullpage('#fullpage', {
-      verticalCentered: false,
-      autoScrolling:true,
-      scrollHorizontally: true
+      // verticalCentered: false,
+      autoScrolling: true,
+      scrollHorizontally: true,
+      onLeave: (origin, destination, direction, trigger) => {
+        switch (origin.index + 1) {
+          case 1:
+            if(direction === 'up') {
+              // 不存在
+            }
+            if(direction === 'down') {
+              this.$refs['cell-2'].style = 'top: 0';
+            }
+            break;
+          case 2:
+            if(direction === 'up') {
+              this.$refs['cell-2'].style = `top: ${viewportHeight1 + 56}px`;
+            }
+            if(direction === 'down') {
+              this.$refs['cell-3'].style = 'top: 0';
+            }
+            break;
+          case 3:
+            if(direction === 'up') {
+              this.$refs['cell-3'].style = `top: ${viewportHeight2 + 56}px`;
+            }
+            if(direction === 'down') {
+              this.$refs['cell-4'].style = 'top: 0';
+            }
+            break;
+          case 4:
+            if(direction === 'up') {
+              this.$refs['cell-4'].style = `top: ${viewportHeight3 + 56}px`;
+            }
+            if(direction === 'down') {
+              this.$refs['cell-5'].style = 'top: 0';
+            }
+            break;
+          case 5:
+            if(direction === 'up') {
+              this.$refs['cell-5'].style = `top: ${viewportHeight4 + 56}px`;
+            }
+            if(direction === 'down') {
+              // 不存在
+            }
+            break;
+        }
+      },
     });
   },
   onLoad(query) {
@@ -35,12 +107,18 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.page-index {
+
+.cells {
   width: 100vw;
   height: 100vh;
-  overflow-y: scroll;
+  position: fixed;
+  border: 2px red solid;
+  top: 0;
+  left: 0;
+  pointer-events: none;
 
   .cell {
+    pointer-events: auto;
     width: 100%;
     height: 56px;
     border: 1px red solid;
@@ -56,50 +134,8 @@ export default {
     letter-spacing: -0.6635649800300598px;
     text-align: left;
 
-    position: sticky;
-    top: 0;
-  }
-
-  .cell-2 {
-    bottom: calc(56px * 3);
-  }
-
-  .cell-3 {
-    bottom: calc(56px * 2);
-  }
-
-  .cell-4 {
-    bottom: calc(56px * 1);
-  }
-
-  .cell-5 {
-    bottom: calc(56px * 0);
-  }
-
-  .content {
-    img {
-      width: 100%;
-    }
-  }
-
-  .A {
-    border: 1px red solid;
-  }
-
-  .B {
-    border: 1px red solid;
-  }
-
-  .C {
-    border: 1px red solid;
-  }
-
-  .D {
-    border: 1px red solid;
-  }
-
-  .E {
-    border: 1px red solid;
+    position: fixed;
+    transition: all 700ms ease-in-out;
   }
 }
 </style>
