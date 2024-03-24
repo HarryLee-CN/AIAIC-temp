aa745cc960d22fdbb280f6ef0403b0e5b25df949
李秋奇@home <mail@qiuqi.li>
2024-03-24 20:23:30 +0800

    no message

--- src/components/Content-2-my-works.vue
+++ src/components/Content-2-my-works.vue
@@ -30,7 +30,11 @@ export default defineComponent({
           查看海报>
         </div>
       </div>
+      <div class="works">
+        <img :src="image" v-for="image in item.images" alt="work"/>
+      </div>
     </div>
+    <img class="submit-again" src="../static/img/btn-submit-again.png" alt="submit-again">
   </div>
 </template>
 
@@ -42,6 +46,7 @@ export default defineComponent({
   align-items: center;
   position: relative;
   min-height: 573px;
+  padding-top: 21px;
 
   .bg {
     width: 25px;
@@ -53,13 +58,13 @@ export default defineComponent({
 
   .block {
     width: 100%;
-    border: 1px red solid;
 
     .title {
       display: flex;
       justify-content: space-between;
       align-items: center;
       padding-right: 37px;
+      margin-bottom: 15px;
 
       .left {
         display: flex;
@@ -91,6 +96,31 @@ export default defineComponent({
         color: #191919;
       }
     }
+
+    .works {
+      margin-bottom: 25px;
+      padding: 0 36px;
+      display: flex;
+      flex-wrap: wrap;
+
+      img {
+        width: 168px;
+        height: 168px;
+        object-position: center center;
+        object-fit: cover;
+        margin-right: 7px;
+        margin-bottom: 7px;
+
+        &:nth-child(2n) {
+          margin-right: 0;
+        }
+      }
+    }
+  }
+
+  .submit-again {
+    width: 329px;
+    margin: -14px auto 24px auto;
   }
 }
 </style>
