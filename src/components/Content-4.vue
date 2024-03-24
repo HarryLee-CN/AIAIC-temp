a1b04304a48c9f182a8ccd402c53a65ce173b80c
李秋奇@home <mail@qiuqi.li>
2024-03-24 19:16:01 +0800

    no message

--- src/components/Content-4.vue
+++ src/components/Content-4.vue
@@ -8,6 +8,7 @@ export default defineComponent({
 
 <template>
   <div class="content-4">
+    <img class="bg" src="../static/img/bg-counselor.png" alt="bg">
     <div class="swiper">
       <div class="swiper-item" v-for="(item, index) in 4">
         <img src="../static/img/counselor.png" alt="counselor">
@@ -19,13 +20,26 @@ export default defineComponent({
 <style scoped lang="scss">
 
 .content-4 {
+  height: calc(100vh - 56px * 2);
+  position: relative;
+  display: flex;
+
+  .bg {
+    width: 110px;
+    position: absolute;
+    left: 0;
+    height: 100%;
+    z-index: -1;
+  }
 
   .swiper {
+    width: 100%;
+    height: 390px;
     margin-top: 125.5px;
     padding-left: 20px;
     padding-right: 20px;
     display: flex;
-    width: fit-content;
+    overflow-x: scroll;
 
     .swiper-item {
       display: flex;
@@ -33,6 +47,7 @@ export default defineComponent({
 
       img {
         width: 284px;
+        height: 390px;
       }
 
       &:last-child {
