981e745838460b85e16b7036ab3f34f43f3d5524
李秋奇@home <mail@qiuqi.li>
2024-03-25 13:57:08 +0800

    no message

--- src/utils/common.js
+++ src/utils/common.js
@@ -37,23 +37,20 @@ export function phoneNumValid(phoneNumber) {
 
 export function imgUpload(ossTokenData, file) {
   return new Promise((resolve) => {
-    const host = ossTokenData.sign.host;
-    const formData = new FormData();
-    formData.append('key', ossTokenData.sign.dir + '/' + ossTokenData.file_name); //存储在oss的文件路径
-    formData.append('OSSAccessKeyId', ossTokenData.sign.accessid); //accessKeyId
-    formData.append('policy', ossTokenData.sign.policy); //policy
-    formData.append('Signature', ossTokenData.sign.signature); //签名
-    formData.append('success_action_status', 200); //成功后返回的操作码
-    formData.append("file", file);
-    /** 通过ajax获取数据 **/
-    uni.request({
-      url: host,
-      data: formData,
-      method: "post",
-      withCredentials: true,
-      success: (res) => {
-        resolve(res)
+    uni.uploadFile({
+      url: ossTokenData.sign.host,
+      name: "file",
+      file,
+      formData: {
+        key: ossTokenData.sign.dir + '/' + ossTokenData.file_name,
+        OSSAccessKeyId: ossTokenData.sign.accessid,
+        policy: ossTokenData.sign.policy,
+        signature: ossTokenData.sign.signature,
+        success_action_status: 200
       },
-    })
+      success: (uploadFileRes) => {
+        resolve(uploadFileRes)
+      }
+    });
   })
 }
